2d9b44a37aad7af42d8af98382f538ce74910fa7
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2022-05-22 17:19:23 -0400

    Update PrimeFactorization.java

--- PrimeFactorization.java
+++ PrimeFactorization.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+// Put in number, gives prime factorization
+// 12 -> 2 2 3 b/c 2 x 2 x 3 = 12
 public class PrimeFactorization {
   public static void factor(long num, ArrayList<Long> smallPrimes, int i, StringBuilder factors) {
     if (num % 2 == 0) {
@@ -9,6 +11,10 @@ public class PrimeFactorization {
       return;
     }
     int sqrt = (int) Math.sqrt(num);
+    // l1 short for loop 1; not the best name but I use it an awful lot
+    // Useful for continue-ing
+    // int i = 0 omitted b/c I don't want to keep resetting it to 0
+    // Everything in a for loop parentheses can be omitted to be for(;;) which would effectively be the same as while (true)
     l1: for (; i <= sqrt; i += 2) {
       for (int j = 0; j < smallPrimes.size() - 1; j++) {
         if (i % smallPrimes.get(j) == 0) {
@@ -24,13 +30,13 @@ public class PrimeFactorization {
         return;
       }
     }
-        /* Condensed version of
-        if (num != 1 || factors.length() == 0) {
-            System.out.println(num);
-        } else {
-            System.out.println();
-        }
-         */
+		/* Condensed version of 
+		if (num != 1 || factors.length() == 0) {
+			System.out.println(num);
+		} else {
+			System.out.println();
+		}
+		 */
     factors.append((num != 1 || factors.length() == 0) ? num : "");
     System.out.println(factors);
   }
@@ -41,7 +47,7 @@ public class PrimeFactorization {
     long num;
     while (true) {
       input = scan.nextLine();
-      // Checks if String is integer
+      // Checks if String is int
       // Don't know how it works, I just modified what I saw from stackoverflow
       if (!input.matches("-?\\d+(\\d+)?")) {
         break;
