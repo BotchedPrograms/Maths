63672e9137335936ee869bd6ca7942b639eb38a9
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2022-05-22 17:20:32 -0400

    Update MultiplicativePersistence.java

--- MultiplicativePersistence.java
+++ MultiplicativePersistence.java
@@ -7,16 +7,16 @@ import java.util.Scanner;
 // 3 times
 // Not much to write home about here
 public class MultiplicativePersistence {
-    public static void multiply(String number, int times) {
-        long product = 1;
-        if (number.length() != 1) {
-            for (int i = 0; i < number.length(); i++) {
-                product *= Integer.parseInt(number.substring(i, i+1));
-            }
-            System.out.println(product);
-            multiply(String.valueOf(product), times + 1);
-        } else {
-            System.out.println(times + " times");
-        }
+  public static void multiply(String number, int times) {
+    long product = 1;
+    if (number.length() != 1) {
+      for (int i = 0; i < number.length(); i++) {
+        product *= Integer.parseInt(number.substring(i, i+1));
+      }
+      System.out.println(product);
+      multiply(String.valueOf(product), times + 1);
+    } else {
+      System.out.println(times + " times");
     }
+  }
 }
