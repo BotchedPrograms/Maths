8d682d4ecac7bac9dae17d8dc130fcd13280fa7d
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2022-05-23 17:02:27 -0400

    Update and rename Dec2Fact.java to BaseFactorial.java

--- BaseFactorial.java
+++ BaseFactorial.java
@@ -1,6 +1,6 @@
-// Base-10 to Base-Factorial
-  // Base-Factorial "has applications in combinatorics and crpytography" so that's cool
-public class Dec2Fact {
+// Base-10 to Base-Factorial and vice versa
+  // "Factorial base representation has applications in combinatorics and crpytography" so that's cool
+public class BaseFactorial {
   // Gets factorial
     // Seems to be generally faster than recursion; point taken
   public static long factorial(int nb) {
