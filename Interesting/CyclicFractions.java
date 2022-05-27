fd93d3087d7b47d23d00706e7028e0a90ab99417
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2022-05-27 12:19:30 -0400

    Update CyclicFractions.java

--- Interesting/CyclicFractions.java
+++ Interesting/CyclicFractions.java
@@ -1,19 +1,20 @@
 import java.util.ArrayList;
 
-// 1/7 is cyclic b/c all its multiples have the same repeating part but starting in different places
+// 1/7 is cyclic b/c all its multiples have the same repeating part, but they start in different places
   // 1/7 = 0.142857
   // 2/7 = 0.285714
   // 3/7 = 0.428571
   // 4/7 = 0.571428
   // 5/7 = 0.714285
   // 6/7 = 0.857142
+// Divides numbers and lists cyclic denominators like 7
 public class CyclicFractions {
-  // Divides a by b in base 10
+  // Prints a/b in base 10
   public static void divide(int a, int b) {
     divide(a, b, 10);
   }
   
-  // Divides a by b in given base
+  // Prints a/b in given base
   public static void divide(int a, int b, int base) {
     System.out.print(a/b);
     System.out.print(".");
@@ -28,6 +29,7 @@ public class CyclicFractions {
   // Gets repeating part of a/b
   public static ArrayList<Integer> getDigits(int a, int b, int base) {
     ArrayList<Integer> digits = new ArrayList<Integer>();
+    // b-1 b/c there are only that many numbers from [1, b) that the remainder can be
     int[] remainders = new int[b-1];
     for (int i = 0; i < b-1; i++) {
       a *= base;
@@ -67,6 +69,8 @@ public class CyclicFractions {
   public static ArrayList<Integer> getCyclics(int start, int end, int base) {
     ArrayList<Integer> cyclics = new ArrayList<Integer>();
     for (int i = start; i <= end; i++) {
+      // Number is cyclic if remainders go through all possible values from [1, i)
+        // i.e. if repeating part is i-1 digits long
       if (getDigits(1, i, base).size() == i-1) {
         cyclics.add(i);
       }
