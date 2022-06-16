ab178a0272d8b934177520ffac24d4329c27182e
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2022-06-16 16:38:28 -0400

    Adds explanation

--- Interesting/ChineseRemainderTheorem.java
+++ Interesting/ChineseRemainderTheorem.java
@@ -2,7 +2,8 @@
 public class ChineseRemainderTheorem {
   // Gets smallest positive number that have certain remainders when divided by certain numbers
     // 3 mod 5, 1 mod 7, and 6 mod 8 --> 78 b/c it's the smallest number under those parameters
-  // Prerequisite: All mods are relatively coprime
+  // Explanation at the bottom
+  // Prerequisite: All mods are relatively prime
   public static int getAnswer(int[] remainders, int[] mods) {
     // N in video
     int product = 1;
@@ -33,7 +34,7 @@ public class ChineseRemainderTheorem {
     return sum % product;
   }
   
-  // Returns true if all the numbers are relatively coprime, false otherwise
+  // Returns true if all the numbers are relatively prime, false otherwise
     // Not necessarily that there wouldn't be an answer, but that the theorem wouldn't work
   public static boolean hasAnswer(int[] remainders, int[] mods) {
     if (remainders.length != mods.length) {
@@ -66,3 +67,26 @@ public class ChineseRemainderTheorem {
     System.out.println(getAnswer(remainders, mods));
   }
 }
+/* 3 mod 5, 1 mod 7, 6 mod 8
+b mod n
+
+bi   Ni   xi   bi * Ni * xi
+3    56    1       168
+1    40    3       120
+6    35    3       210
+
+Ni is the product of all the n's except ni
+  For i = 1, it means that N1 = something mod n1, 0 mod n2, and 0 mod n3
+  Technically N1 is congruent to something mod n1... not equal
+    I'm lazy though, so that's what we're going with
+xi, by definition, is some number such that Ni * xi = 1 mod n1
+  For i = 1, it means bi * Ni * xi = b1 mod n1, 0 mod n2, and 0 mod n3
+All the bi * Ni * xi now look something like
+  i = 1:  b1 mod n1 = 0 mod n2  = 0 mod n3
+  i = 2:  0 mod n1  = b2 mod n2 = 0 mod n3
+  i = 3:  0 mod n1  = 0 mod n2  = b3 mod n3
+The sum mod n1 = (b1 mod n1) + (0 mod n1) + (0 mod n1) = b1 mod n1
+  sum mod n2 = (0 mod n2) + (b2 mod n2) + (0 mod n2) = b2 mod n2
+  sum mod n3 = (0 mod n3) + (0 mod n3) + (b3 mod n3) = b3 mod n3
+Moding whole thing by n1 * n2 * n3 preserves the mods
+*/
