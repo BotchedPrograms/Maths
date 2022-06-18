ddc0cdcb56f2e72b4811de82ab26d754a474cc1b
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2022-06-18 19:07:06 -0400

    Moved import statement back above summary

--- Interesting/Relatively Boring/MultiplicativePersistence.java
+++ Interesting/Relatively Boring/MultiplicativePersistence.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // 2694
   // -> 2*6*9*4 = 432
   // -> 4*3*2 = 24
@@ -5,19 +7,34 @@
   // 3 times
 // Smallest number with highest times that has been found so far (as far as I know) is 277777788888899
 // Not much to write home about here
-import java.util.Scanner;
 
 public class MultiplicativePersistence {
-  public static void multiply(String number, int times) {
+  public static void multiplyOut(String number, int times) {
     long product = 1;
     if (number.length() != 1) {
       for (int i = 0; i < number.length(); i++) {
         product *= Integer.parseInt(number.substring(i, i+1));
       }
       System.out.println(product);
-      multiply(String.valueOf(product), times + 1);
+      multiplyOut(String.valueOf(product), times + 1);
     } else {
       System.out.println(times + " times");
     }
+  }
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String input;
+    System.out.println("Enter a number: ");
+    while (true) {
+      input = scan.nextLine();
+      // Checks if String is integer
+      // Don't know how it works, I just modified what I saw from stackoverflow
+      if (!input.matches("-?\\d+(\\d+)?")) {
+        break;
+      }
+      multiplyOut(input, 0);
+    }
+    scan.close();
   }
 }
