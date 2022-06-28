c115ff3c0e0bd2a158fce9d25437fc98e57ff009
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2022-06-28 14:55:50 -0400

    Put equation in standard conics form
    Or at least I think this is in standard form

--- Interesting/Polynomial Solutions/RotatingParabolas.java
+++ Interesting/Polynomial Solutions/RotatingParabolas.java
@@ -13,7 +13,15 @@ public class RotatingParabolas {
     double d = Math.sqrt((xf-xi)*(xf-xi)+(yf-yi)*(yf-yi))/2;
     double a = 1/(4*d);
     double theta = Math.acos((yf-yi)/(2*d))*Math.abs(xf-xi)/(xf-xi);
-    System.out.println("(y-" + yi + ")*" + Math.cos(theta) + " + (x-" + xi + ")*" + Math.sin(theta) + " = " + a + "((x-" + xi + ")*" + Math.cos(theta) + " - (y-" + yi + ")*" + Math.sin(theta) + ")^2 + " + d);
+    double sin = Math.sin(theta);
+    double cos = Math.cos(theta);
+    double c1 = a*cos*cos;
+    double c2 = -2*a*sin*cos;
+    double c3 = a*sin*sin;
+    double c4 = -sin + 2*a*(-xi*cos*cos + yi*sin*cos);
+    double c5 = -cos + 2*a*(xi*sin*cos - yi*sin*sin);
+    double c6 = d + yi*cos + xi*sin + a*(Math.pow(xi*cos - yi*sin, 2));
+    System.out.printf("%fx^2 + %fxy + %fy^2 + %fx + %fy + %f = 0\n", c1, c2, c3, c4, c5, c6);
   }
 
   public static void main(String[] args) {
