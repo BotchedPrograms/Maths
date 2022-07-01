d50b96524568cd45bc3947697dafcc515b473869
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2022-07-01 15:32:30 -0400

    Added mathematical explanations

--- Interesting/Polynomial Solutions/RotatingParabolas.java
+++ Interesting/Polynomial Solutions/RotatingParabolas.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 
-// I'll add explanations, quality-of-life changes, and more later
-  // For now, just run this command, ctrl c+v the equation to desmos, and go "woah that's so cool"
-// Parabola math at the bottom
+// Mathematical explanations at the bottom
 
 public class RotatingParabolas {
   // Rotates parabola of form y = a(x-h)^2 + k by angle counter-clockwise around origin
@@ -25,34 +23,38 @@ public class RotatingParabolas {
   }
 
   // Input directrix of form y = mx + k1 and focal point of coords (xf, yf) to get corresponding parabola
-    // To fact-check parabola:
-      // Ctrl c+v to desmos
-      // Graph y = mx + k1 and (xf,yf)
-      // Graph y = (-1/m)x + k2 which should be the line perpendicular to the directrix that goes through the focal point
-      // First of all, new line should seem to split parabola in half
-      // Relatively notably, intersection point btwn new line and parabola should be right btwn the intersection of the two lines and the focal point
-        // That's b/c one way to define a parabola is as the set of points with the same distance from a line and a point
+  // To fact-check parabola:
+    // Ctrl c+v to desmos
+    // Graph y = mx + k1 and (xf,yf)
+    // Graph y = (-1/m)x + k2 which should be the line perpendicular to the directrix that goes through the focal point
+    // First of all, new line should seem to split parabola in half
+    // Relatively notably, intersection point btwn new line and parabola should be right btwn the intersection of the two lines and the focal point
+      // That's b/c one way to define a parabola is as the set of points with the same distance from a line and a point
   public static void printEquation(double m, double k1, double xf, double yf) {
-    double k2 = (m*yf+xf)/m;
-    double xi = m*(k2-k1)/(m*m+1);
-    double yi = m*xi+k1;
-    double d = Math.sqrt((xf-xi)*(xf-xi)+(yf-yi)*(yf-yi))/2;
-    double a = 1/(4*d);
-    double angle = Math.acos((yf-yi)/(2*d))*Math.abs(xf-xi)/(xf-xi);
-    double sin = Math.sin(angle);
-    double cos = Math.cos(angle);
+    // Variable names aren't named with much thought for clarity, just what I jot down when I was working this out
+    double k2 = (m*yf+xf)/m;  // y-intercept of perpendicular
+    double xi = m*(k2-k1)/(m*m+1);  // x of intersection of line and perpendicular
+    double yi = m*xi+k1;  // y of intersection of line and perpendicular
+    double d = Math.sqrt((xf-xi)*(xf-xi)+(yf-yi)*(yf-yi))/2;  // Distance between vertex and focal point
+    double a = 1/(4*d);   // The a in f(x) = a(x-h)^2 + k
+    // sin and cos are sin(theta) and cos(theta) where theta is the angle we're rotating
+      // We don't need theta by itself though, so we'll just use its sin and cos
+      // Real reason's b/c the theta used here is hard to put mathematically; you're much better off just putting it this way
+        // Explained in "Solving for 0"
+    double sin = (xf-xi)/(2*d);
+    double cos = (yf-yi)/(2*d);
     double c1 = a*cos*cos;
     double c2 = -2*a*sin*cos;
     double c3 = a*sin*sin;
     double c4 = -sin + 2*a*(-xi*cos*cos + yi*sin*cos);
     double c5 = -cos + 2*a*(xi*sin*cos - yi*sin*sin);
-    double c6 = d + yi*cos + xi*sin + a*(Math.pow(xi*cos - yi*sin, 2));
+    double c6 = d + yi*cos + xi*sin + a*(Math.pow(-xi*cos + yi*sin, 2));
     System.out.printf("%fx^2 + %fxy + %fy^2 + %fx + %fy + %f = 0\n", c1, c2, c3, c4, c5, c6);
   }
 
   // Input directrix of form y = mx + k1 and focal point of coords (xf, yf) to get corresponding parabola
     // Same as previous method but has int inputs and outputs
-    // Int outputs is crazy enough, but how it seems to always start with 1x^2 - I don't even know
+      // Int outputs is crazy enough, but how it seems to always start with 1x^2 - I don't even know
       // Don't make the numbers too big or it won't work
   public static void printEquation(int m, int k1, int xf, int yf) {
     double k2 = (double) (m*yf+xf)/m;
@@ -70,8 +72,8 @@ public class RotatingParabolas {
     long[] coeffs2 = new long[6];
     for (int i = 0; i < 6; i++) {
       // Math.round b/c round-off errors; coeffs[i]*lcd should be a whole number
-        // should, as in I did a few tests, it worked, and I'm praying it's true in general
-        // my math seems to tell me that lcd should be Math.pow(m*m+1, 3) but it seems to work perfectly without it, so I'm not pushing
+        // "should" as in I did a few tests, it worked, and I'm praying it's true in general
+        // My math seems to tell me that lcd should be something else, but it seems to work perfectly fine without it, so I'm not pushing
       coeffs2[i] = Math.round(coeffs[i]*lcd);
     }
     long gcf = gcf(coeffs2);
@@ -161,13 +163,20 @@ public class RotatingParabolas {
   }
 
   public static void main(String[] args) {
-    printEquation(2, -5, 4, -4);
+    printEquation(15, 5, 3, 2);
   }
 }
 
 /*
+Backstory
+  I was reading a textbook and one of the problems involved doing a lot of math, which ultimately spat out an equation like x^2 + 12xy + 36y^2 - 404x - 130x + 659 = 0
+    For context, this section was on conics, which includes equations like x^2 + y^2 = 1 which is a circle and other similar equations for ellipses, hyperbolas, and parabolas
+  I had trouble finding what this equation would look like, so I went to the answer sheet and it was like "you do some math and you get the equation x^2 + 12xy + ... and here's what it looks like:" and it's damn a rotated parabola
+  I was already having a hard time for this whole section, so I've just been nodding my head going "yup, uh huh, that makes sense"
+    But this was the breaking point where I said "no, you can't just drop something like that and expect me to accept it; ima go work this out myself"
+
 Focal points
-  Background info: https://youtu.be/hoh4TmPzu1w
+  Background info: https://youtu.be/owVwjr6pTqc
   To find the focal point of any parabola, let's replace the 2x with f'(x)
     If we use f(x) = ax^2 + bx + c, things get messy with b
     Instead we'll use f(x) = a(x-h)^2 + k, which replaces b with a vertical and horizontal transformation of y = ax^2
@@ -181,7 +190,119 @@ Focal points
     tan(20) = sin(20)       2sin(0)cos(0)       cos^2(0)       2sin(0)/cos(0)         2tan(0)
              --------- = ------------------- / ---------- = --------------------- = ------------
               cos(20)     cos^2(0)-sin^2(0)     cos^2(0)     1-sin^2(0)/cos^2(0)     1-tan^2(0)
-    d      4a^2*(x-h)^2 - 1                 1                x-h       1
-  ----- = ------------------ = a(x-h) - ---------     d = --------- = ----
-   x-h          4a(x-h)                  4a(x-h)           4a(x-h)     4a
+    d      4a^2*(x-h)^2 - 1                 1
+  ----- = ------------------ = a(x-h) - ---------
+   x-h          4a(x-h)                  4a(x-h)
+                     x-h                  1
+  d = a(x-h)^2 - --------- = a(x-h)^2 - ----
+                   4a(x-h)               4a
+  distance from focal point = a(x-h)^2 - d = a(x-h)^2 - (a(x-h)^2 - 1/(4a)) = 1/(4a)
+
+Rotating things
+  Before we rotate a whole parabola, let's work out what rotating a single point looks like mathematically
+         *' (x',y')
+        /
+     h /      __-* (x,y)
+      / b __--
+     /__-- a
+    C ------------------
+    Where * is the original point, *' is the rotated one, a is the angle * is rotated from the horizontal, b is the angle rotated from angle a, C is the center of rotation, and h is the distance between the center and * (or *')
+      sin(a) = y/h and cos(a) = x/h, so x = h*cos(a) and y = h*sin(a)
+      sin(a+b) = y'/h and cos(a+b) = y'/h, so x' = h*cos(a+b) and y' = h*sin(a+b)
+      x' = h*(cos(a)cos(b) - sin(a)sin(b)) = h*cos(a)cos(b) - h*sin(a)sin(b) = x*cos(b) - y*sin(b)
+      y' = h*(sin(a)cos(b) + cos(a)sin(b)) = h*sin(a)cos(b) + h*cos(a)sin(b) = y*cos(b) + x*sin(b)
+    Thus, if we rotate * by an angle 0, we get the equations
+      x' = x*cos(0) - y*sin(0)
+      y' = y*cos(0) + x*sin(0)
+    If we say that C has coordinates (xc, yc), these equations become
+      x'-xc = (x-xc)*cos(0) - (y-yc)*sin(0)
+      y'-yc = (y-yc)*cos(0) + (x-xc)*sin(0)
+  Now, because we're working with a parabola, we set y = a(x-h)^2 + k
+      x' = x*cos(0) - (a(x-h)^2 + k)*sin(0)
+      y' = (a(x-h)^2 + k)*cos(0) + x*sin(0)
+    What can we do with this? Not much (or at least not easily), so what we actually do is set y' = a(x'-h)^2 + k
+      y' = a(x'-h)^2 + k
+      y*cos(0) + x*sin(0) = a((x*cos(0) - y*sin(0) - h)^2 + k
+    From here, if you know 0, you can simplify this to an equation in the form of ax^2 + bxy + cy^2 + dx + ey + f = 0 (zero this time, not theta)
+    Important note: recall how x and y are the old points and x' and y' are the new ones
+      By making y' be an upright parabola and solving for x and y, we make the new parabola upright and solve for the old one
+      i.e. we don't get an upright parabola rotated by 0, but a parabola rotated by 0 to become upright
+      i.e. the parabola is rotated the opposite way, meaning we can solve this issue by just multiplying 0 by -1
+        It's why the method where angle is given, we do angle *= -1;
+        Not done in the other methods because the math is done with the *= -1 in mind
+  If we want to rotate around a point of coordinates (xc, yc), the equation becomes
+    (y-yc)*cos(0) + (x-xc)*sin(0) + yc = a((x-xc)*cos(0) - (y-yc)*sin(0) - h + xc)^2 + k
+
+Solving for rotated parabola when given the directrix and focal point
+  We use the equation above
+    x and y stay in the final equation
+    a, 0, xc, yc, h, and k we need to solve for
+                    ^       \             _--``              ___ perpendicular
+                    |        \         /``focal point ___---
+                    |         \      /`     v  ___ ---
+                    | theta    \     |  ___ f--
+                    | v         \ __ -v-                ____---``
+                    | 0    ___---i    ^``---____------``
+         y-intercept|__---       ^\  vertex     rotated parabola
+             ___--- b  intersection\
+      ___---        |               \
+                    |                \
+   <----------------o-------------------------------------------->
+              origin|                  \
+                    |                   \
+                    v                    \ directrix
+  Solving for a
+    Distance from focal point (which we'll call d from here on) = 1/(4a) as mentioned in "Focal points"
+      d = 1/(4a)
+      4a = 1/d
+      a = 1/(4d)
+    To solve for d, we use the fact that the vertex (along with the whole parabola) is equidistant from the focal point and some line called the directrix
+    If we draw a line through the focal point and perpendicular to the directrix, the distance between the intersection and the focal point will be 2*d
+    All told, to solve for a:
+      Get perpendicular line using point-slope formula
+      Use system of equations of perpendicular and normal lines to find intersection
+        ohh so that's why it's called a normal line, that's cute
+      Find distance between intersection and focal point using distance formula or pythagorean theorem
+      d = half that
+      a = 1/(4d)
+  Solving for 0
+    0 is the theta you see on the graph
+    We want 0 to be a value from -pi to pi (a full circle)
+      sin, cos, or tan inverse are thus out of the question (since they only cover half a circle)
+    With that in mind, it's actually easier if we define sin(0) and cos(0) since every theta in a circle has a unique combination of sin and cos
+    Ok, I should confess something: the graph's a little misleading
+      The theta used in this program isn't the angle from the perpendicular and a vertical line at the origin, but the one at the intersection
+      I honestly think the math would still work and I did play around with it, but the final equation that I got to work consistently was the one at the intersection
+    With that in mind, we can say sin(0) = o/h = (xf-xi)/(2d)
+        xf for focal point, xi for intersection, 2d for distance between them
+      cos(0) = a/h = (yf-yi)/(2d)
+    One might be worried if this is the 0 we want since we multiplied it by -1 earlier
+      Seeing how both sin and cos would be positive in this case, and that we'd need to rotate this parabola by a positive angle to be upright, yes it's the same 0 as our equations from earlier
+  Solving for xc and yc
+    One option is to have the upright parabola rotate around the y-intercept where the 0 in the graph is
+    But as mentioned earlier, the equations used here revolve around the intersection, so xc = xi and yc = yi
+  Solving for h and k
+    h and k are the horizontal and vertical transformations of the upright parabola
+    If you imagine rotating the parabola in the graph upright, you'll see the upright vertex is at x = xi and y = yi + d (d being the distance between the intersection and the vertex)
+    So h = xi and k = yi + d
+  The equation can now be simplified to
+    (y-yi)*cos(0) + (x-xi)*sin(0) + yi = a((x-xi)*cos(0) - (y-yi)*sin(0) - xi + xi)^2 + yi + d
+    (y-yi)*cos(0) + (x-xi)*sin(0) = a((x-xi)*cos(0) - (y-yi)*sin(0))^2 + d
+    where a, sin(0), cos(0), xi, yi, and d are blah blah blah
+
+Solving for int coefficients for final equation in form of ax^2 + bxy + cy^2 + dx + ey + f = 0
+  To get int coefficients, let's see what's stopping them
+  The biggest thing preventing int coefficients is the a(...)^2 part where the sin and cos are over 2d, that 2d is squared, and a is over 4d
+    Thus it's something / ((2d)^2*(4d)) = something / (16d^3)
+  Another thing is that xi and yi (if you work that out in terms of the given m, k1, a, and b (y = mx+k1 and focal point at (a, b)) is over (m^2+1)
+    Because the coefficient of x and y have a combination of 3 xis and yis, we multiply everything by (m^2+1)^3
+    Except apparently it works with just (m^2+1)^2
+      That part I genuinely can't explain
+      More math later and now it seems like it should be m^3(m^2+1)^3
+  All told, multiply both sides by 16d^3 and work that mess out
+    Then multiply the coefficients by (m^2+1)^2
+    Divide them by the gcf of all the coefficients to simplify
+      Optionally, you can try to find why the coefficient of x^2 always seems to be 1
+      Don't ask me though, can't explain this one either
+      Proofs for both of them might be as simple as more algebra 2 working-it-out, but I've had my fill of math
 */
