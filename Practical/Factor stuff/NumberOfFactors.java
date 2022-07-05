1a59d95b9a258cc79f0a518f26ee839c961bf503
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2022-07-05 01:32:02 -0400

    Clarity

--- Practical/Factor stuff/NumberOfFactors.java
+++ Practical/Factor stuff/NumberOfFactors.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
   // Each factor of 60 is a unique combination of 2^0, 2^1, 2^2 ; 3^0, 3^1 ; and 5^0, 5^1
   // The number of unique combinations for 60 is 3 x 2 x 2 = 12
     // i.e. Number of factors = product of (powers + 1)
-// Program can also return a long[] of those factors
+// Also, the getFactors method here returns a long[] of those factors
   // Not mentioned in file name b/c wasn't sure what it would be called
     // Factors feels too short, Factorization might just be wrong, GetFactors sounds weird, UniqueFactors feels unnecessarily complicated, ...
 
