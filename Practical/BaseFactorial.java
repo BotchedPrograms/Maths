2d99b94045beed7b82419ca9376fba1074fef63f
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2022-07-06 01:17:16 -0400

    Update BaseFactorial.java

--- Practical/BaseFactorial.java
+++ Practical/BaseFactorial.java
@@ -4,24 +4,22 @@
 public class BaseFactorial {
   // Gets factorial
     // Seems to be generally faster than recursion; point taken
-  public static long factorial(int nb) {
+  public static long factorial(int num) {
     long fac = 1;
-    for (int i = 1; i <= nb; i++) {
+    for (int i = 1; i <= num; i++) {
       fac *= i;
     }
     return fac;
   }
 
   // Converts numbers to characters
-  public static char overboard(long nb) {
-    // nb short for number, huh?; damn I was lazy
-      // Nevermind, it was given
-    // Converts nb to letter 10-35 -> (A-Z)
-    if (nb > 9) {
-      return (char) (nb+55);
+  public static char overboard(long num) {
+    // Converts num to letter 10-35 -> (A-Z)
+    if (num > 9) {
+      return (char) (num+55);
     }
-    // Converts nb to digit 0-9 -> (0-9)
-    return (char) (nb+48);
+    // Converts num to digit 0-9 -> (0-9)
+    return (char) (num+48);
   }
 
   // Converts Strings to numbers
@@ -31,29 +29,29 @@ public class BaseFactorial {
   }
 
   // Converts decimal to base-factorial
-  public static String dec2FactString(long nb) {
+  public static String dec2FactString(long num) {
     long i = 1;
     long pro = 1;
-    while (nb > i*pro) {
+    while (num > i*pro) {
       i++;
       pro *= i;
     }
-    String digits = "";
+    StringBuilder digits = new StringBuilder();
     for (long k = i; k > 0; k--) {
-      digits += overboard(nb/pro);
-      nb %= pro;
+      digits.append(overboard(num / pro));
+      num %= pro;
       pro /= k;
     }
-    digits += "0";
-    return digits;
+    digits.append("0");
+    return digits.toString();
   }
 
   // Converts base-factorial to decimal
   public static long factString2Dec(String str) {
-    long nb = 0;
+    long num = 0;
     for (int i = 0; i < str.length(); i++) {
-      nb += overboard(Character.toString(str.charAt(i)))*factorial(str.length()-i-1);
+      num += overboard(Character.toString(str.charAt(i)))*factorial(str.length()-i-1);
     }
-    return nb;
+    return num;
   }
 }
