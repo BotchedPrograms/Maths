196c8c69cbe66a25adee4439f8bad897b772d550
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2022-07-06 11:40:52 -0400

    Fixed typo

--- Practical/BaseFactorial.java
+++ Practical/BaseFactorial.java
@@ -1,5 +1,5 @@
 // Base-10 to Base-Factorial and vice versa
-  // "Factorial base representation has applications in combinatorics and crpytography" so that's cool
+  // "Factorial base representation has applications in combinatorics and cryptography" so that's cool
 
 public class BaseFactorial {
   // Gets factorial
