aacdd1bddd90de644c11469bbdfafa60e4c86e29
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2022-10-04 17:31:33 -0500

    Bug fixes, added getDigits method
    Fixed problem where 1/30 would stop since 10 was a remainder before
    Fixed subsequent problem where 1/4 = 0.250 would be considered cyclic
    
    getDigits returns the decimal part before it starts repeating

--- Interesting/CyclicFractions.java
+++ Interesting/CyclicFractions.java
@@ -14,37 +14,76 @@ public class CyclicFractions {
   public static void divide(int a, int b) {
     divide(a, b, 10);
   }
-  
+
   // Prints a/b in given base
   public static void divide(int a, int b, int base) {
-    System.out.print(a/b);
-    System.out.print(".");
+    StringBuilder sb = new StringBuilder();
+    sb.append(a/b);
+    sb.append(".");
     a %= b;
-    ArrayList<Integer> digits = getDigits(a, b, base);
-    for (int i = 0; i < digits.size(); i++) {
-      System.out.print(overload(digits.get(i)));
+    ArrayList<Integer> digits = getRepeatingDigits(a, b, base);
+    for (Integer digit : digits) {
+      sb.append(overload(digit));
     }
-    System.out.println();
+    System.out.println(sb);
   }
-  
+
   // Gets repeating part of 1/b in base 10
+  public static ArrayList<Integer> getRepeatingDigits(int b) {
+    return getRepeatingDigits(1, b, 10);
+  }
+
+  // Gets repeating part of a/b in base 10
+  public static ArrayList<Integer> getRepeatingDigits(int a, int b) {
+    return getRepeatingDigits(a, b, 10);
+  }
+
+  // Gets repeating part of a/b
+  public static ArrayList<Integer> getRepeatingDigits(int a, int b, int base) {
+    ArrayList<Integer> digits = new ArrayList<>();
+    // b-1 b/c there are only that many numbers from [1, b) that the remainder can be
+    int[] remainders = new int[b-1];
+    int index = 0;
+    for (int i = 0; i < b-1; i++) {
+      a *= base;
+      index = helper(remainders, a % b, digits, a / b, i);
+      if (index == -1) {
+        digits.add(a/b);
+        remainders[i] = a%b;
+      } else {
+        break;
+      }
+      a %= b;
+    }
+    if (index > 0) {
+      digits.subList(0, index).clear();
+    }
+    return digits;
+  }
+
+  // Gets decimal of 1/b in base 10 before it repeats
+    // 1/70 = 0.0142857
+      // 142857 is the repeating bit
+      // 0142857 is the decimal before it repeats
   public static ArrayList<Integer> getDigits(int b) {
     return getDigits(1, b, 10);
   }
-  
-  // Gets repeating part of a/b in base 10
+
+  // Gets decimal of a/b in base 10 before it repeats
   public static ArrayList<Integer> getDigits(int a, int b) {
     return getDigits(a, b, 10);
   }
 
-  // Gets repeating part of a/b
+  // Gets decimal of a/b in give base before it repeats
   public static ArrayList<Integer> getDigits(int a, int b, int base) {
-    ArrayList<Integer> digits = new ArrayList<Integer>();
+    ArrayList<Integer> digits = new ArrayList<>();
     // b-1 b/c there are only that many numbers from [1, b) that the remainder can be
     int[] remainders = new int[b-1];
+    int index;
     for (int i = 0; i < b-1; i++) {
       a *= base;
-      if (!inArray(remainders, i, a%b)) {
+      index = helper(remainders, a % b, digits, a / b, i);
+      if (index == -1) {
         digits.add(a/b);
         remainders[i] = a%b;
       } else {
@@ -55,14 +94,17 @@ public class CyclicFractions {
     return digits;
   }
 
-  // Returns true if n is arr from index 0 to given, false otherwise
-  public static boolean inArray(int[] arr, int index, int n) {
+  public static int helper(int[] remainders, int mod, ArrayList<Integer> digits, int quo, int index) {
+    // Made to stop 1/4 = 0.250 being considered cyclic, having a 0 digit and 0 mod makes it return early
+    if (mod == 0 && quo == 0) {
+      return 0;
+    }
     for (int i = 0; i < index; i++) {
-      if (arr[i] == n) {
-        return true;
+      if (remainders[i] == mod && digits.get(i) == quo) {
+        return i;
       }
     }
-    return false;
+    return -1;
   }
 
   // Turns n to character
@@ -78,27 +120,28 @@ public class CyclicFractions {
 
   // Returns cyclic denominator from start to end in given base
   public static ArrayList<Integer> getCyclics(int start, int end, int base) {
-    ArrayList<Integer> cyclics = new ArrayList<Integer>();
+    ArrayList<Integer> cyclics = new ArrayList<>();
     for (int i = start; i <= end; i++) {
       // Number is cyclic if remainders go through all possible values from [1, i)
-        // i.e. if repeating part is i-1 digits long
-      if (getDigits(1, i, base).size() == i-1) {
+      // i.e. if repeating part is i-1 digits long
+      if (getRepeatingDigits(1, i, base).size() == i-1) {
         cyclics.add(i);
       }
     }
     return cyclics;
   }
 
-  // Prints ArrayList
-  public static void print(ArrayList<Integer> arr) {
-    for (int i = 0; i < arr.size(); i++) {
-      System.out.print(arr.get(i));
-      System.out.print(" ");
+  // Prints an int array
+  public static void print(int[] arr) {
+    StringBuilder sb = new StringBuilder();
+    for (int num : arr) {
+      sb.append(num);
+      sb.append(" ");
     }
-    System.out.println();
+    System.out.println(sb);
   }
-  
+
   public static void main(String[] args) {
-    print(getCyclics(1, 100));
+    System.out.println(getCyclics(3, 100));
   }
 }
