2de43c5058abc5e9e0b3d757a5911dd73fc74abc
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2022-12-27 19:11:37 -0500

    Update NegativeBases.java

--- Practical/Based stuff/NegativeBases.java
+++ Practical/Based stuff/NegativeBases.java
@@ -1,43 +1,48 @@
-import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class NegativeBases {
-  public static BigInteger[] divide(BigInteger num, BigInteger den) {
-    BigInteger a = num.divide(den);
-    BigInteger b = num.subtract(a.multiply(den));
-    return new BigInteger[] {a, b};
-  }
+  public static String toNegative(int num, int base) {
+    ArrayList<Integer> arr = new ArrayList<>();
+    int radix = -1;
+    String answer = null;
+    while (!(radix >= 0 && radix < Math.abs(base))) {
+      MixedRadix mr = new MixedRadix(arr);
+      answer = mr.toRadix(num);
 
-  public static String convertDecTo(BigInteger num, int baseTo) {
-    StringBuilder sb = new StringBuilder();
-    BigInteger bT = BigInteger.valueOf(baseTo);
-    BigInteger[] bIs;
-    while (num.compareTo(BigInteger.ZERO) != 0) {
-      bIs = divide(num, bT);
-      sb.append(intToChar(Math.abs(bIs[1].intValue())));
-      num = bIs[0];
+      String[] toRadix = answer.split(" ");
+      radix = Integer.parseInt(toRadix[0]);
+      arr.add(base);
     }
-    return sb.reverse().toString();
+    return clean(answer);
   }
 
-  public static char intToChar(int digit) {
-    if (bounded(digit, 0, 9)) return (char) (digit+48);  // Turns 0-9 to '0'-'9'
-    if (bounded(digit, 10, 35)) return (char) (digit+87);  // Turns 10-35 to 'a'-'z'
-    if (bounded(digit, 36, 61)) return (char) (digit+29);  // Turns 36-61 to 'A'-'Z'
-    char[] characters = {'!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '_', '+', '[', ']', '\\', '{', '}', '|', ';', '\'', ':', '"', ',', '.', '/', '<', '>', '?', '`', '~'};
-    if (bounded(digit, 62, 93)) return characters[digit-62];  // Turns other ints to other characters
-    return ' ';
+  public static int fromNegative(String num, int base) {
+    int sum = 0;
+    int product = 1;
+    for (int i = num.length() - 1; i >= 0; i--) {
+      sum += Integer.parseInt(num.substring(i, i + 1)) * product;
+      product *= base;
+    }
+    return sum;
   }
 
-  public static boolean bounded(int num, int min, int max) {
-    return num >= min && num <= max;
+  public static String clean(String toClean) {
+    if (toClean == null) {
+      return null;
+    }
+
+    StringBuilder sb = new StringBuilder();
+    char[] charArr = toClean.toCharArray();
+    for (char letter : charArr) {
+      if (letter != ' ') {
+        sb.append(letter);
+      }
+    }
+    return sb.toString();
   }
 
   public static void main(String[] args) {
-    for (int i = -42; i < 6; i++) {
-      BigInteger b1 = BigInteger.valueOf(i);
-      BigInteger b2 = BigInteger.valueOf(-7);
-      BigInteger[] arr = divide(b1, b2);
-      System.out.println(convertDecTo(b1, -7));
-    }
+    System.out.println(toNegative(15532, -8));
+    System.out.println(fromNegative("42334", -8));
   }
 }
