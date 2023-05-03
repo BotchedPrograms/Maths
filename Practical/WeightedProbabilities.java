d48df8259310631c07696a8e718d8208d13e5b2b
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2023-05-03 19:20:31 -0400

    Renovated WeightedProbabilities.java

--- Practical/WeightedProbabilities.java
+++ Practical/WeightedProbabilities.java
@@ -1,72 +1,179 @@
-// getItem gets random String from String[] with weights for how likely they pop up from int[]
+import java.util.*;
 
-public class WeightedProbabilities {
-  public static String getItem(String[] items, int[] weights) {
-    // Catches errors
-      // Rare from me, I know
-    if (items.length == 0 || weights.length == 0) {
-      return null;
-    }
-    if (items.length != weights.length) {
-      return null;
-    }
-    // Weights of 1 2 3 4 --> 1 3 6 10
-      // Where sumWeights[n] = sum of weights of index <= n
-    long[] sumWeights = new long[weights.length];
-    long sum = 0;
-    for (int i = 0; i < weights.length; i++) {
-      sumWeights[i] = sum;
-      sum += weights[i];
-    }
-    // Using 1 2 3 4 as example again, gives random number from 0 to 9 inclusive
-    long random = (long) (Math.random() * sum);
-    int start = 0;
-    int end = sumWeights.length;
-    int index = -1;
-    int times = (int) (Math.log(weights.length) / Math.log(2)) + 1;
-    // Binary-ish search where it goes find the index where the current number is <= to it and the next is >
-      // Overcomplicating stuff maybe, but now this is fairly efficient
-    for (int i = 0; i <= times; i++) {
-      index = (start + end) / 2;
-      // Breaks if it reaches ends
-      if (index == 0 || index == sumWeights.length - 1) {
-        break;
-      }
-      if (sumWeights[index] <= random && sumWeights[index + 1] > random) {
-        break;
-      }
-      if (sumWeights[index] <= random) {
-        start = index;
-      } else {
-        end = index;
-      }
-    }
-    return items[index];
-  }
+// Most efficient when only put() on new values and getRandom() are called
+public class WeightedProbabilities<V> {
+    private final TreeMap<Integer, V> tree;
+    private final HashMap<V, Integer> hash;
+    private final Random rand = new Random();
+    private int max;
 
-  // Gets index of String in String[] or -1 if it's not there
-  public static int getIndexOf(String[] arr, String item) {
-    for (int i = 0; i < arr.length; i++) {
-      if (arr[i].equals(item)) {
-        return i;
-      }
+    /**
+     * Creates new WeightedProbabilities, which facilitates getting values with weighted probabilities.
+     * Note: Works best when using only put() on new values and getRandom().
+     * remove(), updateWeight(), and getWeights() presumably work but aren't very efficient.
+     */
+    public WeightedProbabilities() {
+        tree = new TreeMap<>();
+        hash = new HashMap<>();
+        max = 0;
     }
-    return -1;
-  }
 
-  public static void main(String[] args) {
-    String[] items = {"I", "don't", "even", "know", "if", "this", "actually", "works", "or", "not"};
-    int[] weights =  {9  , 1      , 9     , 2     , 6   , 3     , 1         , 7      , 7   , 0    };
-    int sum = 0;
-    for (int weight : weights) {
-      sum += weight;
+    /**
+     * Puts in the new value with the specified weight.
+     * Note: The program won't work properly if you put in something already put in, so don't do that.
+     *
+     * @throws IllegalArgumentException if weight is not positive.
+     */
+    public void put(V value, int weight) {
+        if (weight <= 0) {
+            throw new IllegalArgumentException();
+        }
+        max += weight;
+        tree.put(max - 1, value);
+        hash.put(value, max - 1);
     }
-    int[] times = new int[items.length];
-    for (int i = 0; i < sum*Math.pow(10, 6); i++) {
-      times[getIndexOf(items, getItem(items, weights))]++;
+
+    /**
+     * Removes a value. Assumes there's only one of it.
+     *
+     * @return the removed value if it was there, null otherwise
+     */
+    public V remove(V value) {
+        Integer weight = hash.remove(value);
+        if (weight == null) {
+            return null;
+        }
+        V removed = tree.remove(weight);
+        Map.Entry<Integer, V> lighter = tree.lowerEntry(weight);
+        // where "real" weights are the ones put in while "fake" ones are the ones
+        // the program works with
+        int realWeight = weight;
+        if (lighter == null) {
+            realWeight += 1;
+        } else {
+            realWeight -= lighter.getKey();
+        }
+        max -= realWeight;
+        updateHeavier(weight, weight - realWeight);
+        return removed;
     }
-    for (int time : times) {
-      System.out.printf("%,9d\n", time);
+
+    /**
+     * Updates the weight of a value to the one specified.
+     *
+     * @throws IllegalArgumentException if weight is not positive
+     */
+    public void updateWeight(V value, int newWeight) {
+        if (newWeight <= 0) {
+            throw new IllegalArgumentException();
+        }
+        int oldWeight = hash.get(value);
+        Map.Entry<Integer, V> lighter = tree.lowerEntry(oldWeight);
+        int realWeight = oldWeight;
+        if (lighter == null) {
+            realWeight += 1;
+        } else {
+            realWeight -= lighter.getKey();
+        }
+        int fakeNewWeight = oldWeight - realWeight + newWeight;
+        max -= realWeight;
+        max += newWeight;
+        updateHeavier(oldWeight, fakeNewWeight);
     }
-  }
+
+    /**
+     * Updates the weights of all values that had weight greater than equal to oldWeight.
+     * Note that oldWeight and newWeight are "fake" not "real" as defined earlier.
+     */
+    private void updateHeavier(int oldWeight, int newWeight) {
+        int diff = newWeight - oldWeight;
+        Set<Integer> heavierWeights = new HashSet<>(tree.tailMap(oldWeight, true).keySet());
+        for (Integer heavierWeight : heavierWeights) {
+            V heavy = tree.remove(heavierWeight);
+            tree.put(heavierWeight + diff, heavy);
+            hash.replace(heavy, heavierWeight + diff);
+        }
+    }
+
+    /**
+     * Returns a random value with the specified weighted probabilities.
+     */
+    public V getRandom() {
+        return tree.ceilingEntry(rand.nextInt(max)).getValue();
+    }
+
+    /**
+     * Returns the set of values.
+     */
+    public Set<V> getValues() {
+        return hash.keySet();
+    }
+
+    /**
+     * Returns the set of weights.
+     */
+    public List<Integer> getWeights() {
+        Set<Integer> weights = tree.keySet();
+        List<Integer> returnedWeights = new LinkedList<>();
+        if (weights.size() == 0) {
+            return returnedWeights;
+        }
+        Iterator<Integer> iter = weights.iterator();
+        int next = iter.next();
+        returnedWeights.add(next + 1);
+        while (iter.hasNext()) {
+            int curr = next;
+            next = iter.next();
+            returnedWeights.add(next - curr);
+        }
+        return returnedWeights;
+    }
+
+    /**
+     * Returns the total weight.
+     */
+    public int getTotalWeight() {
+        return max;
+    }
+
+    public static void main(String[] args) {
+        WeightedProbabilities<String> barterRates = new WeightedProbabilities<>();
+        barterRates.put("Enchanted Book", 5);
+        barterRates.put("Iron Boots", 8);
+        barterRates.put("Fire Res Splash Pot", 8);
+        barterRates.put("Fire Res Pot", 8);
+        barterRates.put("Water Bottle", 10);
+        barterRates.put("Iron Nugget", 10);
+        barterRates.put("Ender Pearl", 10);
+        barterRates.put("String", 20);
+        barterRates.put("Nether Quartz", 20);
+        barterRates.put("Obsidian", 40);
+        barterRates.put("Crying Obsidian", 40);
+        barterRates.put("Fire Charge", 40);
+        barterRates.put("Leather", 40);
+        barterRates.put("Soul Sand", 40);
+        barterRates.put("Nether Brick", 40);
+        barterRates.put("Spectral Arrow", 40);
+        barterRates.put("Gravel", 40);
+        barterRates.put("Blackstone", 40);
+        String[] drops = {"Enchanted Book", "Iron Boots", "Fire Res Splash Pot", "Fire Res Pot", "Water Bottle",
+            "Iron Nugget", "Ender Pearl", "String", "Nether Quartz", "Obsidian", "Crying Obsidian", "Fire Charge",
+            "Leather", "Soul Sand", "Nether Brick", "Spectral Arrow", "Gravel", "Blackstone"};
+        int[] nums = new int[drops.length];
+        // 10^7
+        int times = 10000000;
+        for (int i = 0; i < times; i++) {
+            String rand = barterRates.getRandom();
+            int index = 0;
+            for (; index < drops.length; index++) {
+                if (rand.equals(drops[index])) {
+                    break;
+                }
+            }
+            nums[index]++;
+        }
+        for (int num : nums) {
+            System.out.println((double) num / times);
+        }
+    }
 }
