a5aa6f63eb38b437b218b7c09e52d0bcbbdb77ae
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2023-08-17 00:30:15 -0400

    Update Totient.java
    Use more efficient algorithm for calculating the totient of a number using its prime factors

--- Practical/Factor and Prime stuff/Totient.java
+++ Practical/Factor and Prime stuff/Totient.java
@@ -1,75 +1,39 @@
-import java.util.ArrayList;
+import java.util.List;
 
 // The toitent function counts the number of non-relatively primes from 1 to n
-  // For n = 12:
-    // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
-    // 1, 5, 7, 11 aren't relatively prime to 12 so the totient function gives 4
+// For n = 12:
+// 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+// 1, 5, 7, 11 aren't relatively prime to 12 so the totient function gives 4
 // Also equals n * (1 - 1/p1) * (1 - 1/p2) * ... where ps are the unique prime factors of n
-  // For n = 12:
-    // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
-    //    2     4     6     8     10      12      removing multiples of 2 / removing half of numbers
-    //       3                 9                  removing multiples of 3 / removing remaining third of numbers
-    // 1           5     7            11          what's left is thus 12 * (1 - 1/2) * (1 - 1/3) = 4
+// For n = 12:
+// 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+//    2     4     6     8     10      12      removing multiples of 2 / removing half of numbers
+//       3                 9                  removing multiples of 3 / removing remaining third of numbers
+// 1           5     7            11          what's left is thus 12 * (1 - 1/2) * (1 - 1/3) = 4
 // Made by Euler (b/c of course) and is used in number theory and encryption
 
 public class Totient {
-  public static int totient(long n) {
-    if (n < 1) {
-      return 0;
+    public static long totient(long num) {
+        if (num < 1) {
+            return 0;
+        }
+        long product = num;
+        List<Long> primeFactors = PrimeFactorization.factor(num);
+        PrimeFactorization.removeDuplicates(primeFactors);
+        for (Long primeFactor : primeFactors) {
+            product /= primeFactor;
+            product *= primeFactor - 1;
+        }
+        return product;
     }
-    double product = n;
-    ArrayList<Long> primes = removeDuplicates(factor(n));
-    for (long prime : primes) {
-      if (n % prime == 0) {
-        product *= 1 - 1.0/prime;
-        n /= prime;
-      }
-    }
-    return (int) product;
-  }
 
-  // Gets prime factors
-  public static ArrayList<Long> factor(long num) {
-    return factor(num, 3, new ArrayList<>());
-  }
-
-  public static ArrayList<Long> factor(long num, long i, ArrayList<Long> factors) {
-    if (num % 2 == 0) {
-      if (num == 0) {
-        return factors;
-      }
-      factors.add(2L);
-      return factor(num/2, i, factors);
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.printf("%2d ", totient(i));
+            if (i % 10 == 0) {
+                System.out.println();
+            }
+        }
+        System.out.println(totient(93884313611L));
     }
-    long sqrt = (long) Math.sqrt(num);
-    for (; i <= sqrt; i += 2) {
-      if (num % i == 0) {
-        factors.add(i);
-        return factor(num/i, i, factors);
-      }
-    }
-    if (num != 1) {
-      factors.add(num);
-    }
-    return factors;
-  }
-
-  // Removes duplicates from arr
-  public static ArrayList<Long> removeDuplicates(ArrayList<Long> arr) {
-    for (int i = arr.size() - 1; i > 0; i--) {
-      if (arr.get(i).equals(arr.get(i - 1))) {
-        arr.remove(i);
-      }
-    }
-    return arr;
-  }
-
-  public static void main(String[] args) {
-    for (int i = 1; i <= 100; i++) {
-      System.out.printf("%2d ", totient(i));
-      if (i % 10 == 0) {
-        System.out.println();
-      }
-    }
-  }
 }
