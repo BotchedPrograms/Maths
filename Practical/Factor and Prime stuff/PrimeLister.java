2d1878ba2f0e073115df363de3b48aaef5a252bf
Andy Li <103963170+BotchedPrograms@users.noreply.github.com>
2023-09-22 01:04:36 -0400

    Update PrimeLister.java
    Made method return just list
    Uses linkedlist to make use of its deque properties
    Fixed bug where it forgets to list 3

--- Practical/Factor and Prime stuff/PrimeLister.java
+++ Practical/Factor and Prime stuff/PrimeLister.java
@@ -1,39 +1,40 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 // Lists primes, that's it
 
 public class PrimeLister {
-  // Lists prime <= max
-  public static ArrayList<Long> listPrimes(long max) {
-    ArrayList<Long> primes = new ArrayList<>();
-    // Goes through odd numbers from 3 to max inclusive
-      // Even numbers obviously divisible by 2
-        // Except 2 ofc, which is added later so we don't need to check if odd number is divisible by it
-    l1: for (long i = 3; i <= max; i += 2) {
-      // Checks if number is divisible by known primes
-      for (long prime : primes) {
-        if (i % prime == 0) {
-          continue l1;
+    // Lists prime <= max
+    public static List<Long> listPrimes(long max) {
+        LinkedList<Long> primes = new LinkedList<>();
+        // Goes through odd numbers from 3 to max inclusive
+            // Even numbers obviously divisible by 2
+                // Except 2 ofc, which is added later so we don't need to check if odd number is divisible by it
+        l1: for (long i = 3; i <= max; i += 2) {
+            // Checks if number is divisible by known primes
+            for (long prime : primes) {
+                if (i % prime == 0) {
+                    continue l1;
+                }
+                // If it's not a prime, there should be a prime factor <= sqrt
+                    // Thus breaks and adds it to primes if it can't find one
+                if (prime > Math.sqrt(i)) {
+                    break;
+                }
+            }
+            primes.addLast(i);
         }
-        // If it's not a prime, there should be a prime factor <= sqrt
-          // Thus breaks and adds it to primes if it can't find one
-        if (prime > Math.sqrt(i)) {
-          break;
+        // Adds 2 to start of primes
+        if (max >= 2) {
+            primes.addFirst(2L);
         }
-      }
-      primes.add(i);
+        return primes;
     }
-    // Adds 2 to start of primes
-    if (max >= 2) {
-      primes.set(0, 2L);
-    }
-    return primes;
-  }
 
-  public static void main(String[] args) {
-    // Only takes around 1.5 seconds to list primes from 1 - 10,000,000
-      // Wow, so fast!
-    ArrayList<Long> primes = listPrimes(10000000);
-    System.out.println(primes);
-  }
+    public static void main(String[] args) {
+        // Only takes around 1.5 seconds to list primes from 1 - 10,000,000
+            // Wow, so fast!
+        List<Long> primes = listPrimes(10000000);
+        System.out.println(primes);
+    }
 }
